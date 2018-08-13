bce67d8a77434407682fa2529a4f60e83e45f2f5
Konrad Bartczak <konradbartczak@onet.pl>
2018-08-13 12:51:56 +0200

    update

--- src/main/java/com/konrad/school/AppConfig.java
+++ src/main/java/com/konrad/school/AppConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
 
 @Configuration
 @ComponentScan(basePackages = "com.konrad.school")
@@ -17,22 +17,28 @@ import java.beans.PropertyVetoException;
 public class AppConfig {
     @Autowired
     private Environment env;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
     @Bean
     public DataSource securityDataSource() {
-        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
         try {
-            comboPooledDataSource.setDriverClass(env.getProperty("spring.datasource.driver-class-name"));
+            securityDataSource.setDriverClass(env.getProperty("spring.datasource.driver-class-name"));
         } catch (PropertyVetoException e) {
             throw new RuntimeException(e);
         }
-        comboPooledDataSource.setJdbcUrl(env.getProperty("spring.datasource.url"));
-        comboPooledDataSource.setUser(env.getProperty("spring.datasource.username"));
-        comboPooledDataSource.setPassword(env.getProperty("spring.datasource.password"));
-        comboPooledDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
-        comboPooledDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
-        comboPooledDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
-        comboPooledDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
-        return comboPooledDataSource;
+
+        logger.info("\n\n\n\n\n" + env.getProperty("spring.datasource.username") + "\n\n\n\n");
+
+        securityDataSource.setJdbcUrl(env.getProperty("spring.datasource.url"));
+        securityDataSource.setUser(env.getProperty("spring.datasource.username"));
+        securityDataSource.setPassword(env.getProperty("spring.datasource.password"));
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+        return securityDataSource;
     }
 
     private int getIntProperty(String propName) {
