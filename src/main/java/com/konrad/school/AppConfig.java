1a4628a2816e52a3c02fb2ad49ffe90f4a59dac2
Konrad Bartczak <konradbartczak@onet.pl>
2018-08-16 15:27:01 +0200

    Configuration for jpa queries.

--- src/main/java/com/konrad/school/AppConfig.java
+++ src/main/java/com/konrad/school/AppConfig.java
@@ -6,7 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
 import java.util.logging.Logger;
@@ -17,18 +23,37 @@ import java.util.logging.Logger;
 public class AppConfig {
     @Autowired
     private Environment env;
+    // Spring Data JPA repositories configuration
+    @Bean
+    public DataSource dataSource () {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+    }
+    @Bean
+    public EntityManagerFactory entityManagerFactory(){
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.konrad.school");
+        factory.setDataSource(dataSource());
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
 
     //private Logger logger = Logger.getLogger(getClass().getName());
 
     @Bean
     public DataSource securityDataSource() {
+
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
         ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
         try {
             securityDataSource.setDriverClass(env.getProperty("spring.datasource.driver-class-name"));
         } catch (PropertyVetoException e) {
             throw new RuntimeException(e);
         }
-
         //logger.info("\n\n\n\n\n" + env.getProperty("spring.datasource.username") + "\n\n\n\n");
 
         securityDataSource.setJdbcUrl(env.getProperty("spring.datasource.url"));
